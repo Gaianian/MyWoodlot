713f1dc62b236df0589cdcb0ecb73da03c434324
MikeTschudi <mtschudi@esri.com>
2014-11-17 09:12:59 -0800

    Updated Vietnamese language file

--- js/nls/vi/resources.js
+++ js/nls/vi/resources.js
@@ -20,49 +20,49 @@ define(
             error: "Impossible de créer la carte"
         },
         tooltips: {
-            search: "Rechercher",  // Command button to open a dialog box for finding a feature or an address (depending on app)
-            locate: "Emplacement actuel",  // Command button to zoom and pan to the current geographical position reported by the browser
-            markup: "Envoyer une correction",  // Command button to submit a correction to the app's host
-            collect: "Filtrer/Modifier",  // Command button to open a filter and template picker to add features to the map and to edit them afterwards
-            dijitLegend: "Afficher la légende",  //Display the legend
-            filter: "Filtrer les couches de carte",  // Explains purpose of type-in box affiliated with template picker
-            basemap: "Changer de fond de carte",  // Command button to open a dialog box for switching basemaps
-            share: "Partager",  // Command button to open a dropdown menu for picking a type of sharing
-            shareViaEmail: "Partager par courrier électronique",  // Command button to share the current map extents via email
-            shareViaFacebook: "Partager via Facebook",  // Command button to share the current map extents via a Facebook post: a URL is opened that permits the user to log into Facebook with a post that is ready to go
-            shareViaTwitter: "Partager via Twitter",  // Command button to share the current map extents via a Twitter tweet: a URL is opened that permits the user to log into Twitter with a tweet that is ready to go
-            print: "Imprimer la carte",  // Command button to open a dialog box for specifying print orientation, title, and author before printing; also used inside print dialog box
-            fetchPrint: "Afficher la carte imprimée",  // Command button to open a PDF containing a map that was just printed by the print map command
-            landscape: "Orientation de page Paysage",  // Command button in the print map dialog box to select the landscape page orientation
-            portrait: "Orientation de page Portrait",  // Command button in the print map dialog box to select the portrait page orientation
-            help: "Aide"  // Command button to open a dialog box with a short description of the app
+            search: "Tìm",  // Command button to open a dialog box for finding a feature or an address (depending on app)
+            locate: "Vị trí hiện tại",  // Command button to zoom and pan to the current geographical position reported by the browser
+            markup: "Đệ trình hiệu chỉnh",  // Command button to submit a correction to the app's host
+            collect: "Lọc/Sửa",  // Command button to open a filter and template picker to add features to the map and to edit them afterwards
+            dijitLegend: "Hiển thị chú giải",  //Display the legend
+            filter: "Lọc lớp bản đồ",  // Explains purpose of type-in box affiliated with template picker
+            basemap: "Chuyển bản đồ nền",  // Command button to open a dialog box for switching basemaps
+            share: "Chia sẻ",  // Command button to open a dropdown menu for picking a type of sharing
+            shareViaEmail: "Chia sẻ qua email",  // Command button to share the current map extents via email
+            shareViaFacebook: "Chia sẻ qua Facebook",  // Command button to share the current map extents via a Facebook post: a URL is opened that permits the user to log into Facebook with a post that is ready to go
+            shareViaTwitter: "Chia sẻ qua Twitter",  // Command button to share the current map extents via a Twitter tweet: a URL is opened that permits the user to log into Twitter with a tweet that is ready to go
+            print: "In bản đồ",  // Command button to open a dialog box for specifying print orientation, title, and author before printing; also used inside print dialog box
+            fetchPrint: "Xem bản đồ đã in",  // Command button to open a PDF containing a map that was just printed by the print map command
+            landscape: "Hướng trang ngang",  // Command button in the print map dialog box to select the landscape page orientation
+            portrait: "Hướng trang dọc",  // Command button in the print map dialog box to select the portrait page orientation
+            help: "Trợ giúp"  // Command button to open a dialog box with a short description of the app
         },
         labels: {
-            email: "Email",  // Shown next to icon for sharing the current map extents via email; works with shareViaEmail tooltip
+            email: "email",  // Shown next to icon for sharing the current map extents via email; works with shareViaEmail tooltip
             Facebook: "Facebook",  // Shown next to icon for sharing the current map extents via a Facebook post; works with shareViaFacebook tooltip
             Twitter: "Twitter",  // Shown next to icon for sharing the current map extents via a Twitter tweet; works with shareViaTwitter tooltip
-            title: "titre",  // Shown as title hint in print specification box if a title hint is not configured
-            author: "auteur"  // Shown as author hint in print specification box if an author hint is not configured
+            title: "tiêu đề",  // Shown as title hint in print specification box if a title hint is not configured
+            author: "tác giả"  // Shown as author hint in print specification box if an author hint is not configured
         },
         prompts: {
-            search: "Rechercher :",  // Appears before a find text field in dialog box for searching for a feature
-            markup: "Dessiner",  // Appears before a set of tools for drawing on the map
-            mapLayers: "Couches de carte :",  // Appears before a list of map layers; shown when the app is not configured with the layer to use for the find command; works with the searchLayerMissing message
-            layerFields: "Rechercher les champs de la couche :"  // Appears before a list of fields in the configured map find layer; shown when the app cannot find one or more of the fields that were configured for the find command; works with the searchFieldMissing message
+            search: "Tìm:",  // Appears before a find text field in dialog box for searching for a feature
+            markup: "Vẽ",  // Appears before a set of tools for drawing on the map
+            mapLayers: "Lớp bản đồ:",  // Appears before a list of map layers; shown when the app is not configured with the layer to use for the find command; works with the searchLayerMissing message
+            layerFields: "Tìm trường lớp:"  // Appears before a list of fields in the configured map find layer; shown when the app cannot find one or more of the fields that were configured for the find command; works with the searchFieldMissing message
         },
         messages: {
-            geolocationDenied: "Ce site ne détient pas d\'autorisation pour obtenir l\'emplacement actuel",  // Shown when the browser does not permit the app to get the current geographical position
-            geolocationUnavailable: "Le navigateur n\'a pas pu obtenir l\'emplacement actuel",  // Shown when the browser returns an error instead of the current geographical position
-            geolocationTimeout: "Le navigateur n\'a pas pu obtenir l\'emplacement actuel à temps",  // Shown when the browser does not return within a configured time limit when asked for the current geographical position
-            noSearchLayerConfigured: "Aucune couche permettant d\'effectuer des recherches n\'a été configurée",  // Appears before a list of map layers; shown when the app is not configured with any layers to use for the find command; works with the mapLayers prompt
-            searchLayerMissing: "Cette couche de recherche est introuvable sur la carte",  // Appears before a list of map layers; shown when the app is not configured with the layer to use for the find command; works with the mapLayers prompt
-            searchLayerNotSearchable: "Les champs sont introuvables pour la couche.<br><br>Vérifiez que cette couche est présente à la racine du contenu de la carte. Les services imbriqués, tels que les services de carte dynamiques ArcGIS for Server, doivent être ajoutés à la carte couche par couche (y compris le numéro d\'index de couche) pour être utilisés comme couches de recherche. Les services tuilés ne peuvent pas être utilisés comme couches de recherche.",
-            searchFieldMissing: "Ce champ est introuvable dans la couche de recherche de la carte",  // Appears before a list of fields in the configured map find layer; shown when the app cannot find one or more of the fields that were configured for the find command; works with the layerFields prompt
-            allSearchFieldsMissing: "Aucun de ces champs n\'a été trouvé dans la couche de recherche de la carte",  // Appears before a list of fields in the configured map find layer; shown when the app cannot find any of the fields that were configured for the find command; works with the layerFields prompt
-            fieldNotFound: "Ce champ n\'existe dans aucune couche de la carte",  // Appears when a field used in the configuration was not found in any map layer
-            yourContentSubmitted: "Votre contenu a été envoyé. Merci.",  // Appears after content has been added to the map and successfully submitted to the server
-            noConfiguration: "Impossible d\'accéder à la configuration de l\'application",  // Appears if the app, during startup, cannot get access to or find the configuration information; without the information, it cannot build the UI
-            unableToLaunchApp: "Impossible de lancer l\'application"  // Appears for any failure to build the user interface
+            geolocationDenied: "Trang web này không được phép nhận vị trí hiện tại",  // Shown when the browser does not permit the app to get the current geographical position
+            geolocationUnavailable: "Trình duyệt này không thể nhận vị trí hiện tại",  // Shown when the browser returns an error instead of the current geographical position
+            geolocationTimeout: "Trình duyệt này không thể nhận vị trí hiện tại kịp thời",  // Shown when the browser does not return within a configured time limit when asked for the current geographical position
+            noSearchLayerConfigured: "Đ_No find layer has been configured_ớ",  // Appears before a list of map layers; shown when the app is not configured with any layers to use for the find command; works with the mapLayers prompt
+            searchLayerMissing: "Không tìm thấy lớp cần tìm trong bản đồ",  // Appears before a list of map layers; shown when the app is not configured with the layer to use for the find command; works with the mapLayers prompt
+            searchLayerNotSearchable: "Không thể tìm thấy các trường cho lớp bản đồ.<br><br>Xác nhận rằng lớp này tồn tại ở thư mục gốc của Nội dung bản đồ. Dịch vụ lồng nhau, chẳng hạn như dịch vụ bản đồ năng động ArcGIS cho Máy chủ phải được thêm vào một lớp bản đồ tại một thời điểm (bao gồm cả số chỉ số lớp) được sử dụng để Tìm Lớp. Không thể sử dụng dịch vụ lát để Tìm Lớp.",
+            searchFieldMissing: "Không tìm thấy trường này trong lớp bản đồ cần tìm",  // Appears before a list of fields in the configured map find layer; shown when the app cannot find one or more of the fields that were configured for the find command; works with the layerFields prompt
+            allSearchFieldsMissing: "Không tìm thấy trường nào trong các trường này trong lớp bản đồ cần tìm",  // Appears before a list of fields in the configured map find layer; shown when the app cannot find any of the fields that were configured for the find command; works with the layerFields prompt
+            fieldNotFound: "Trường này không tồn tại trong bất kỳ lớp bản đồ nào",  // Appears when a field used in the configuration was not found in any map layer
+            yourContentSubmitted: "Nội dung của bạn đã được đệ trình. Xin cảm ơn.",  // Appears after content has been added to the map and successfully submitted to the server
+            noConfiguration: "Không thể truy cập cấu hình ứng dụng",  // Appears if the app, during startup, cannot get access to or find the configuration information; without the information, it cannot build the UI
+            unableToLaunchApp: "Không thể khởi chạy ứng dụng"  // Appears for any failure to build the user interface
         }
     })
 );
