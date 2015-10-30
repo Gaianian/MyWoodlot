8d0c83eaa2fe8cf8b7a2c261245b1d413ad165c4
MikeTschudi <mtschudi@esri.com>
2015-10-30 05:38:22 -0700

    Removed obsolete click event handling

--- js2/lgonlineMap.js
+++ js2/lgonlineMap.js
@@ -559,10 +559,9 @@ define([
          * (http://arcgis4localgov2.maps.arcgis.com/home/item.html?id=f232cac140a8495f9990cc9d2bb66dd9)
          */
         enablePopups: function () {
-            // Not usable until we've created the map
-            if (this.appConfig.mapInfo.clickEventListener) {
-                this.appConfig.mapInfo.clickEventHandle = on(this.appConfig.map, "click", this.appConfig.mapInfo.clickEventListener);
-            }
+            // OK to see the map's info window
+            // https://developers.arcgis.com/javascript/jsapi/map-amd.html#setinfowindowonclick
+            this.appConfig.map.setInfoWindowOnClick(true);
         },
 
         /**
@@ -573,10 +572,6 @@ define([
             // We don't want to see the map's info window
             // https://developers.arcgis.com/javascript/jsapi/map-amd.html#setinfowindowonclick
             this.appConfig.map.setInfoWindowOnClick(false);
-            // Not usable until we've created the map
-            if (this.appConfig.mapInfo.clickEventHandle) {
-                this.appConfig.mapInfo.clickEventHandle.remove();
-            }
         }
     });
 
